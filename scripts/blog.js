03f26bae60cfb7d5cffd25bda43311ffefe8a109
Siddhant Mishra <siddhant.mishra@hotmail.com>
2017-09-17 10:41:52 +0530

    Blog and project styling update

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,10 +1,38 @@
-var postDB={};
-$.getJSON("posts/posts.json", function(json){
-    postDB=json;
-} )
 $('document').ready(function(){
     console.log(postDB.numberOfPosts)
 });
 function loadPosts(category){
-    console.log(category, " ", postDB.numberOfPosts);
+    console.log(category, " ");
+    switch(category){
+        case 'latest':{
+
+            break;
+        }
+        case 'technology':{
+            $.get("/posts/1.html", function( my_var ) {
+                console.log(my_var);
+            });
+            break;
+        }
+    }
 }
+var postDB={
+    "numberOfPosts": 1,
+    "latest":{
+        "number": 1,
+        "fileNames":["1"]
+    },
+    "technology":{
+        "number": 1,
+        "fileNames":["1"]
+    },
+    "lifestyle":{
+        "number": 0,
+        "fileNames":[]
+    },
+    "rantsAndRaves":{
+        "number": 0,
+        "fileNames":[]
+    }
+
+};
