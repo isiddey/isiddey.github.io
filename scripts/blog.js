c771c1f61141270db61d69a578a9bb43365eb1d1
Siddhant Mishra <siddhant.mishra@hotmail.com>
2017-09-18 10:28:27 +0530

    Complete blog Script update

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,38 +1,75 @@
-$('document').ready(function(){
-    console.log(postDB.numberOfPosts)
-});
-function loadPosts(category){
-    console.log(category, " ");
-    switch(category){
-        case 'latest':{
+var postDB = {};
+$.getJSON("posts/posts.json", function (json) {
+    postDB = json;
+})
 
+function loadPosts(category) {
+    console.log(category, " ");
+    switch (category) {
+        case 'latest':
+            {
+                var recent = postDB.numberOfPosts > 6 ? postDB.numberOfPosts - 6 : postDB.numberOfPosts;
+                var title = "";
+                var content = "";
+                var postImage = "";
+                var counter = 0;
+                var changedHTML = "";
+                while (recent > 0) {
+                    $.get("posts/" + recent + ".json", function (lVar) {
+                        console.log(lVar)
+                        postImage = lVar.image;
+                        title = lVar.title;
+                        content = lVar.content.length > 150 ? lVar.content.substring(0, 150) : lVar.content;
+                    }).done(function () {
+                        changedHTML += '<div class="panel row post"><a href="#"> <img src="images/'+postImage+'"alt="postImage" class="postImage col-lg-4 col-md-4 col-sm-4 col-xs-12"> <div class="postDescription col-lg-8 col-md-8 col-sm-8 col-xs-12"><h3>' + title + '</h3><p>' + content + '....</p></div></a></div>'
+                        counter++;
+                        if (counter == 2) $("#content").html(changedHTML);
+                    });
+                    recent--;
+                }
+                break;
+            }
+        case 'technology':
+            {
+                loadDynamicPosts('technology', 6);
+                break;
+            }
+        case 'lifestyle':{
+            loadDynamicPosts('lifestyle', 6);
             break;
         }
-        case 'technology':{
-            $.get("/posts/1.html", function( my_var ) {
-                console.log(my_var);
-            });
-            break;
+        case 'rantsAndRaves':{
+            loadDynamicPosts('rantsAndRaves', 6);
+        break;
         }
     }
 }
-var postDB={
-    "numberOfPosts": 1,
-    "latest":{
-        "number": 1,
-        "fileNames":["1"]
-    },
-    "technology":{
-        "number": 1,
-        "fileNames":["1"]
-    },
-    "lifestyle":{
-        "number": 0,
-        "fileNames":[]
-    },
-    "rantsAndRaves":{
-        "number": 0,
-        "fileNames":[]
-    }
 
-};
+function loadDynamicPosts(category, highRange) {
+    var totalPosts = postDB[category].number;
+    var lowRange = 1;
+    highRange = highRange > totalPosts ? totalPosts : highRange;
+    lowRange = highRange - 6 > 0 ? highRange - 6 : 1;
+    var title = "";
+    var content = "";
+    var postImage = "";
+    var counter = 0;
+    var changedHTML = totalPosts==0? 'No posts yet. Check soon!': "";
+    console.log(postDB[category].number)
+    for (; lowRange <= highRange; lowRange++) {
+        $.get("posts/" + lowRange + ".json", function (lVar) {
+            console.log(lVar)
+            title = lVar.title;
+            postImage = lVar.image;
+            content = lVar.content.length > 150 ? lVar.content.substring(0, 150) : lVar.content;
+        }).done(function () {
+            changedHTML += '<div class="panel row post"><a href="#"> <img src="images/'+postImage+'"alt="postImage" class="postImage col-lg-4 col-md-4 col-sm-4 col-xs-12"> <div class="postDescription col-lg-8 col-md-8 col-sm-8 col-xs-12"><h3>' + title + '</h3><p>' + content + '....</p></div></a></div>'
+            counter++;
+            if (counter == highRange) {
+                if(highRange > 6) changedHTML += '<button class="left">Previous</button>'
+                if(highRange < totalPosts) changedHTML += '<button class="right">Next</button>'
+                $("#content").html(changedHTML);
+            }
+        });
+    }
+}
